3507189fc4ccedbbcc55f540fb52a17da10faebd
Valmantas Paliksa <walmis@gmail.com>
2019-12-15 20:47:55 +0200

    Fix version.h generation

--- component.mk
+++ component.mk
@@ -7,3 +7,10 @@ COMPONENT_ADD_INCLUDEDIRS := src/platforms/esp8266 src/include src/platforms/com
 COMPONENT_SRCDIRS := src/target src src/platforms/esp8266 src/platforms/common
 CFLAGS += -DNO_LIBOPENCM3=1 -Wno-error=char-subscripts -Wno-char-subscripts -DPROBE_HOST=esp8266
 COMPONENT_OBJEXCLUDE := src/platforms/common/cdcacm.o
+
+$(COMPONENT_PATH)/src/include/version.h: 
+	$(MAKE) -C $(COMPONENT_PATH)/src include/version.h
+    
+build: $(COMPONENT_PATH)/src/include/version.h
+    
+
